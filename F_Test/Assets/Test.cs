8f6f7bc186eb0c934c631da6335e85f5bbeafb48
lion95110 <lion95110@naver.com>
2025-12-04 23:06:59 +0900

    룰렛 시스템
    룰렛 회전 및 감속 구현 및 통합

--- F_Test/Assets/Test.cs
+++ F_Test/Assets/Test.cs
@@ -1,21 +1,40 @@
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+
+
+public class  Player
+{
+    private int hp = 100;
+    private int power = 50;
+    public void Attack()
+    {
+        Debug.Log(this.power + " 데미지를 입혔다!");
+    }
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + " 데미지를 입었다");
+    }
+
+}
 
 public class Test : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+  
     void Start()
     {
-        string name;
-        name = "Sera";
-        Debug.Log(name);
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
 
-
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
